6553a5ff9d20adfc574591cb20dfe58afa90b590
marianaSilva-26 <mariana.silvadossantos.26@gmail.com>
2020-09-24 16:04:15 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace grausCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius;
+            double fahrenheit = 32;
+
+            Console.Write("digite sua temperatura em celsius:");
+            celsius = double.Parse(Console.ReadLine());
+
+            fahrenheit = celsius * 1.8 + 32;
+            Console.WriteLine("celsius para fahrenheit = {0}" , fahrenheit);
+            Console.Write(" /n ");
+
+            Console.WriteLine("pressione Enter para sair!");
+            Console.ReadKey();
+        }
+    }
+}
